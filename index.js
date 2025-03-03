cfa1133eb0b453aa5b0c7278741a5deca51c4bd2
Ian Wafula <neocorpse@proton.me>
2025-03-03 11:53:35 +0300

    Add relevant control flow structures

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet <= 1500) {
+    return 'That will be twenty bucks.'
+  } else if (feet >= 2500) {
+    return 'No can do.'
+  } else {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.': 'No go.';
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
